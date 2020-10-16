4d2219c42721a8b5b634824e0cce9bb8183147ea
Walber Teodoro <walberteo@gmail.com>
2020-10-16 10:41:53 -0300

    movido httpClient para seu arquivo proprio

--- test/data/usecases/load_news/remote_load_news_test.dart
+++ test/data/usecases/load_news/remote_load_news_test.dart
@@ -1,8 +1,10 @@
 import 'package:meta/meta.dart';
 
+import 'package:test/test.dart';
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
-import 'package:test/test.dart';
+
+import 'package:microblogging/data/http/http.dart';
 
 class RemoteLoadNews {
   final HttpClient httpClient;
@@ -15,16 +17,27 @@ class RemoteLoadNews {
   }
 }
 
-abstract class HttpClient {
-  Future<void> request({@required String url, @required String method});
-}
-
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
   RemoteLoadNews sut;
   HttpClientSpy httpClient;
   String url;
+
+  List<Map> mockValidData() => [
+        {
+          "user": {
+            "name": "O Boticário",
+            "profile_picture":
+                "https://pbs.twimg.com/profile_images/1240676323913347074/Gg09hEPx_400x400.jpg"
+          },
+          "message": {
+            "content":
+                "Além disso, nossos funcionários e familiares receberão kits de proteção. Afinal, o cuidado começa aqui dentro, né?",
+            "created_at": "2020-02-02T16:10:33Z"
+          }
+        },
+      ];
 
   setUp(() {
     httpClient = HttpClientSpy();
@@ -33,6 +46,15 @@ void main() {
   });
 
   test('Deve chamar HttpCliente com a url e método correta', () async {
+    await sut.load();
+
+    verify(httpClient.request(url: url, method: 'get'));
+  });
+
+  test('Deve retornar as news em 200', () async {
+    when(httpClient.request(url: anyNamed('url'), method: anyNamed('method')))
+        .thenAnswer((_) async => mockValidData());
+
     await sut.load();
 
     verify(httpClient.request(url: url, method: 'get'));
