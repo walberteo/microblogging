a21784eeeeeb11225112dcb74472d0712057cc67
Walber Teodoro <walberteo@gmail.com>
2020-10-19 11:40:03 -0300

    movido HttpAdapter para seu arquivo próprio

---
+++ lib/infra/http_adapter.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+import '../data/http/http.dart';
+
+class HttpAdapter implements HttpClient {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<Map> request({String url, String method}) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    };
+    var response = Response('', 500);
+    try {
+      if (method == 'get') {
+        response = await client.get(url, headers: headers);
+      }
+    } catch (error) {
+      throw HttpError.serverError;
+    }
+
+    return _handleResponse(response);
+  }
+
+  Map _handleResponse(Response response) {
+    if (response.statusCode == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    } else if (response.statusCode == 204) {
+      return null;
+    } else if (response.statusCode == 400) {
+      throw HttpError.badRequest;
+    } else if (response.statusCode == 401) {
+      throw HttpError.unauthorized;
+    } else if (response.statusCode == 403) {
+      throw HttpError.forbidden;
+    } else if (response.statusCode == 404) {
+      throw HttpError.notFound;
+    } else {
+      throw HttpError.serverError;
+    }
+  }
+}
