e90ffbd474d61576347043ce7b90e047be1be56f
Walber Teodoro <walberteo@gmail.com>
2020-10-19 10:48:36 -0300

    garantido que HttpAdapter lança um ServerError quando chamado um method inválido

---
+++ test/infra/http_adapter_test.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+
+import 'package:faker/faker.dart';
+import 'package:http/http.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import 'package:microblogging/data/http/http.dart';
+
+class HttpAdapter implements HttpClient {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<Map> request({String url, String method}) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    };
+    var response = Response('', 500);
+    try {
+      if (method == 'get') {
+        response = await client.get(url, headers: headers);
+      }
+    } catch (error) {
+      throw HttpError.serverError;
+    }
+
+    return _handleResponse(response);
+  }
+
+  Map _handleResponse(Response response) {
+    if (response.statusCode == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    } else {
+      throw HttpError.serverError;
+    }
+  }
+}
+
+class ClientSpy extends Mock implements Client {}
+
+void main() {
+  HttpAdapter sut;
+  ClientSpy client;
+  String url;
+
+  setUp(() {
+    client = ClientSpy();
+    sut = HttpAdapter(client);
+    url = faker.internet.httpUrl();
+  });
+
+  group('shared', () {
+    test('deve emitir ServerError se um method inválido for informado',
+        () async {
+      final future = sut.request(url: url, method: 'invalid_method');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+  });
+}
