7732df6ee352a0222677e97ade73d415358543ad
Walber Teodoro <walberteo@gmail.com>
2020-10-20 09:35:49 -0300

    movido o carregamento do NewsPage setup para um método helper

--- test/ui/pages/news_page_test.dart
+++ test/ui/pages/news_page_test.dart
@@ -7,9 +7,10 @@ import 'package:mockito/mockito.dart';
 class NewsPresenterSpy extends Mock implements NewsPresenter {}
 
 void main() {
-  testWidgets('deve chamar loadData no carregamento da pagina',
-      (WidgetTester tester) async {
-    final presenter = NewsPresenterSpy();
+  NewsPresenterSpy presenter;
+
+  Future<void> loadPage(WidgetTester tester) async {
+    presenter = NewsPresenterSpy();
     final serverysPage = GetMaterialApp(
       initialRoute: '/news',
       getPages: [
@@ -17,7 +18,11 @@ void main() {
       ],
     );
     await tester.pumpWidget(serverysPage);
+  }
 
+  testWidgets('deve chamar loadData no carregamento da pagina',
+      (WidgetTester tester) async {
+    await loadPage(tester);
     verify(presenter.loadData()).called(1);
   });
 }
